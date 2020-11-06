dff2b4fa11b563e9ab9919287ee418053c177cf5
wakesz87 <kisszab87@gmail.com>
2020-11-06 13:15:28 +0100

    gyakorlás

---
+++ src/main/java/week02/ArrayMain.java
@@ -0,0 +1,18 @@
+package week02;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        int[] a  = new int [3];
+        a[0] = 1;
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+
+        System.out.println(a.length);
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/week02/ArrayMain2.java
@@ -0,0 +1,10 @@
+package week02;
+
+public class ArrayMain2 {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        for (int i = 0; i < a.length; i++){
+    }
+
+}
+}
