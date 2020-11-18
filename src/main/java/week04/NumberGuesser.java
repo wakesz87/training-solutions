73789b34b789d7176c2d0834a1298f193d12e37c
wakesz87 <kisszab87@gmail.com>
2020-11-18 16:51:37 +0100

    Feladat

--- src/main/java/week04/NumberGuesser.java
+++ src/main/java/week04/NumberGuesser.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 
 public class NumberGuesser {
     public static void main(String[] args) {
-        int numn = new Random().nextInt(100) + 1;
+        int numb = new Random().nextInt(100) + 1;
         Scanner scanner = new Scanner(System.in);
-        System.out.println(numn);
+        System.out.println(numb);
         boolean ok = false;
         int round = 0;
         while (!ok && round<=6){
             System.out.println("kérek egy számot");
             int guessed=Integer.parseInt(scanner.nextLine());
-            if(guessed>numn){
+            if(guessed>numb){
                 System.out.println("kisebb");
-            } else if(guessed<numn){
+            } else if(guessed<numb){
                 System.out.println("nagyobb");
             }else{
                 System.out.println("Talált");
