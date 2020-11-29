b4d69ce075f296767b9c811695f451de30ee99e1
wakesz87 <kisszab87@gmail.com>
2020-11-29 16:25:01 +0100

    Feladat

--- src/test/java/week05d03/ListCounterTest.java
+++ src/test/java/week05d03/ListCounterTest.java
@@ -3,32 +3,31 @@ package week05d03;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListCounterTest {
+
     @Test
-    public void startWithATest() {
+    void counterStartATestA() {
         ListCounter listCounter = new ListCounter();
-     //   ArrayList testlist = new ArrayList<> ("Alma", "Körte, Ananász");
-      //  assertEquals(2, listCounter.counterVowels(testlist));
-
-
-
-
-}
-}
-
- /*   @Test
-    public void testsitution(){
-        List<String> testList = Arrays.asList("Alma", "Korte", "avocado", "kukorica", "abanán", "málna");
-        assertEquals(3, counter(testList, c);
+        List<String> list = Arrays.asList("alma", "Körte", "áfonya", "Ablak");
+        assertEquals(2, listCounter.find(list));
     }
 
-
+    @Test
+    void counterStartATestB() {
+        ListCounter listCounter = new ListCounter();
+        List<String> list = Arrays.asList("falma", "Körte", "áfonya", "BAblak");
+        assertEquals(0, listCounter.find(list));
+    }
 
     @Test
-    public void testsitutionNullList(){
-        List<String> testList = new ArrayList<>();
-        assertEquals(0, counter(testList, c);*/
+    void counterStartATestWithEmptyList() {
+        ListCounter listCounter = new ListCounter();
+        List<String> list = new ArrayList<>();
+        assertEquals(0, listCounter.find(list));
+    }
+}
