592a07dbf0319745b7ae2fd8e54b03ebcfe9f82e
wakesz87 <kisszab87@gmail.com>
2020-12-02 16:08:11 +0100

    Feladat

--- src/main/java/week06d03/WordEraser.java
+++ src/main/java/week06d03/WordEraser.java
@@ -26,3 +26,21 @@ public class WordEraser {
         kitörli belőle. A words paraméterben a szavak szóközzel vannak elválasztva. A metódus térjen vissza a törölni kívánt
         szavak nélküli Stringgel. (Mellőzzük a replace és replaceAll használatát)*/
 
+/*
+public class WordEraser {
+
+    public String eraseWord(String words, String word) {
+
+        StringBuilder builder = new StringBuilder();
+        String[] arrWords = words.split(" ");
+
+        for (int i =0; i < arrWords.length; i++) {
+            if (!arrWords[i].equals(word)) {
+                builder.append(arrWords[i] + " ");
+            }
+        }
+        return builder.toString().trim();
+    }
+}
+ */
+
