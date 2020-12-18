5d92e20a649cac01dc9769fa921a7ed60f87a134
wakesz87 <kisszab87@gmail.com>
2020-12-18 12:05:40 +0100

    Feladat

--- src/main/java/week08d05/MathAlgorithms.java
+++ src/main/java/week08d05/MathAlgorithms.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class MathAlgorithms {
 
    public static int  greatestCommonDivisor(int number1, int number2) {
-        if(number2 ==0) {
+        if(number2 == 0) {
             return number1;
         }
         return greatestCommonDivisor(number2, number1 % number2);
