3a6a29b627b31bcffdea459b520782547e751cbf
wakesz87 <kisszab87@gmail.com>
2021-01-06 20:32:01 +0100

    Feladat

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,2 +1,12 @@
 package exam02;
 
+public class ArraySelector {
+
+}
+/* Írj egy olyan metódust az ArraySelector osztályban,
+ mely egy kapott tömbből minden második elemet kiválogat,
+  és egy szövegként összerak, szögletes zárójelek között!
+A páros megállapítása index szerint működik. Azaz az első,
+ nulla indexű elem páros, a második, azaz egyes indexű elem páratlan,
+  stb.
+Ha nulla elemű a tömb, akkor üres stringet adjon vissza!*/
