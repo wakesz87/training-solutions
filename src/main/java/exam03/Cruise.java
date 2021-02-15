aee10374d30728b82d11ea192f62cd77fd89ff4a
wakesz87 <kisszab87@gmail.com>
2021-02-15 20:13:14 +0100

    Feladat

--- src/main/java/exam03/Cruise.java
+++ src/main/java/exam03/Cruise.java
@@ -1,4 +1,29 @@
 package exam03;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cruise {
+    private Boat boat;
+    private LocalDate sailing;
+    private double basicPrice;
+    private List<Passenger> passengers = new ArrayList<>();
+
+    public Cruise(Boat boat, LocalDate sailing, double basicPrice) {
+        this.boat = boat;
+        this.sailing = sailing;
+        this.basicPrice = basicPrice;
+    }
+
+    public void bookPassenger(Passenger passenger) {
+        if (passengers.size() < boat.getMaxPassengers()) {
+            passengers.add(passenger);
+        } else {
+            throw new IllegalArgumentException("there is no empty space");
+        }
+
+    }
 }
+
+
