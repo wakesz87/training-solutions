bcf7d2303f20c0e00e867bc6b4035ba9d83c2e81
wakesz87 <kisszab87@gmail.com>
2021-02-22 19:53:19 +0100

    Feladat

--- src/main/java/exam03retake01/CdvCheck.java
+++ src/main/java/exam03retake01/CdvCheck.java
@@ -1,7 +1,23 @@
 package exam03retake01;
 
 public class CdvCheck {
-
-
-
+    public boolean check(String number) {
+        if (number.length() != 10) {
+            throw new IllegalArgumentException("The number not ten character long!");
+        }
+        int sum = 0;
+        int lastChar;
+        try {
+            lastChar = Integer.parseInt(number.substring(9));
+            for (int i = 0; i < 9; i++) {
+                sum += (i+1) * Integer.parseInt(number.substring(i, i + 1));
+            }
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("Not valid input");
+        }
+        return (sum % 11) == lastChar;
+    }
 }
+
+
+
