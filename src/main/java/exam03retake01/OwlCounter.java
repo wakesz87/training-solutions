95e31e67a1b63fd363e37cbe9ba62fbef4e43ef8
wakesz87 <kisszab87@gmail.com>
2021-02-22 20:11:06 +0100

    Feladat

--- src/main/java/exam03retake01/OwlCounter.java
+++ src/main/java/exam03retake01/OwlCounter.java
@@ -1,5 +1,30 @@
 package exam03retake01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class OwlCounter {
 
+    private Map<String, Integer> owls;
+
+    public OwlCounter() {
+        owls = new HashMap<>();
+    }
+
+    public void readFromFile(BufferedReader reader) throws IOException {
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split("=");
+            owls.put(parts[0], Integer.parseInt(parts[1]));
+        }
+    }
+    public int getNumberOfOwls(String country) {
+        return owls.get(country);
+    }
+
 }
+
+
--- src/test/java/exam03retake01/OwlCounterTest.java
+++ src/test/java/exam03retake01/OwlCounterTest.java
@@ -1,4 +1,4 @@
-ackage exam03retake01;
+package exam03retake01;
 
 import org.junit.jupiter.api.Test;
 
