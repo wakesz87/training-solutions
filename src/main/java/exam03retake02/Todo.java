4b29c0e6e66bbc736b0833032738261b1567b7c9
wakesz87 <kisszab87@gmail.com>
2021-02-26 20:34:28 +0100

    exam03_retake02_Todo

--- src/main/java/exam03retake02/Todo.java
+++ src/main/java/exam03retake02/Todo.java
@@ -1,5 +1,34 @@
 package exam03retake02;
 
 public class Todo {
+    private String text;
+    private State state;
+    private int priority;
 
+    public Todo(String text, int priority) {
+        if (priority < 1 || priority > 5) {
+            throw new IllegalArgumentException(priority + "wrong");
+        }
+
+        this.text = text;
+        this.priority = priority;
+        state = State.NON_COMPLETED;
+
+
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+    public void complete() {
+        this.state = State.COMPLETED;
+    }
 }
