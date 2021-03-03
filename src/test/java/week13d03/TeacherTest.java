72a3b492f7fc1c21437600f9e536e55b3365996b
wakesz87 <kisszab87@gmail.com>
2021-03-03 18:18:15 +0100

    Teacher Calendar

--- src/test/java/week13d03/TeacherTest.java
+++ src/test/java/week13d03/TeacherTest.java
@@ -10,12 +10,12 @@ public class TeacherTest {
     @Test
     public void testTeacher() {
         InputStreamReader isr = new InputStreamReader(TeacherTest.class.getResourceAsStream("/beosztas.txt"));
-        Assertions.assertEquals(23, new Teacher().getNumber(isr, "Kaffer Kada"));
+        Assertions.assertEquals(0, new Teacher().getNumber(isr, "Kaffer Kada"));
 
         InputStreamReader isr2 = new InputStreamReader(TeacherTest.class.getResourceAsStream("/beosztas.txt"));
-        Assertions.assertEquals(24, new Teacher().getNumber(isr2, "Albatrosz Aladin"));
+        Assertions.assertEquals(3, new Teacher().getNumber(isr2, "Albatrosz Aladin"));
 
         InputStreamReader isr3 = new InputStreamReader(TeacherTest.class.getResourceAsStream("/beosztas.txt"));
-        Assertions.assertEquals(22, new Teacher().getNumber(isr3, "Csincsilla Csilla"));
+        Assertions.assertEquals(2, new Teacher().getNumber(isr3, "Csincsilla Csilla"));
     }
 }
