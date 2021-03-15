6eb73bb2591b7b366789c4a2d1774b3c541d323f
wakesz87 <kisszab87@gmail.com>
2021-03-15 18:27:22 +0100

    covid

---
+++ src/main/java/covidproject/CovidMain.java
@@ -0,0 +1,28 @@
+package covidproject;
+
+import java.util.Scanner;
+
+public class CovidMain {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Üdvözöljük az oltás kezelő alkalmazásban!");
+        System.out.println("-------------------------------------------");
+        System.out.println("1. Egyéni regisztráció");
+        System.out.println("2. Csoportos/Tömeges regisztráció");
+        System.out.println("3. Generálás");
+        System.out.println("4. Oltás felvétele ");
+        System.out.println("5. OLtás meghiúsulása");
+        System.out.println("6. Riport");
+        System.out.println("9. Kilépés");
+        String menuBar = sc.nextLine();
+
+
+        if(menuBar.equals("1")){
+            try{
+
+            }
+
+        }
+    }
+}
